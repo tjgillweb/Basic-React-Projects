a8593fc5a828143762ed755bf2be9dfb6382a1b9
tjgillweb <tjgillweb@gmail.com>
2020-12-21 21:41:06 +0530

    Add starter files

---
+++ 02-tours-page/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  
+}
+
+export default App

03ac995ca7151d268b52c6f4c57bd73196521a11
tjgillweb <tjgillweb@gmail.com>
2020-12-21 21:44:44 +0530

    Add Tour cards

--- 02-tours-page/src/App.js
+++ 02-tours-page/src/App.js
@@ -3,7 +3,37 @@ import Loading from './Loading'
 import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  
+  const [tours, setTours] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchTours = async() => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setIsLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if(isLoading){
+    return <main>
+      <Loading />
+    </main>
+  }
+  else {
+    return <main>
+      <Tours tours={tours} />
+    </main>
+  }
+
 }
 
 export default App

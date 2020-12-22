41569d9c1c0e2d528cd599ca09f1285c64d62f1c
tjgillweb <tjgillweb@gmail.com>
2020-12-22 21:46:55 +0530

    change reviews on prev and next buttons

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -5,6 +5,27 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0)
   const {name, job, image, text} = people[index]
+
+  const checkNumber = (number) => {
+    if(number > people.length-1){
+      return 0
+    }
+    if(number < 0){
+      return people.length - 1;
+    }
+    return number
+  }
+  const prevPerson = () => {
+    setIndex((index) => {
+      return checkNumber(index-1)
+    })
+  }
+  const nextPerson = () => {
+    setIndex((index) => {
+      return checkNumber(index+1)
+    })
+  }
+
   return (
     <article className='review'>
       <div className='img-container'>
@@ -17,10 +38,10 @@ const Review = () => {
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='prev-btn'>
+        <button className='prev-btn' onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className='next-btn'>
+        <button className='next-btn' onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>

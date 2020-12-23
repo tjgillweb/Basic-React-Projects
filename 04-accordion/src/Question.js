74ab00478e45981b066a27ca434462c029210bc2
tjgillweb <tjgillweb@gmail.com>
2020-12-23 21:47:34 +0530

    Add accordion toggle functionality

--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>Single Question component</h2>
+
+const Question = ({question}) => {
+  const {id, title, info} = question
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setToggle(!toggle)}>
+          {toggle ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {toggle && <p>{info}</p>}
+    </article>
+  )
 };
 
 export default Question;

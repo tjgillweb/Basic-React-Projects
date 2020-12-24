a8ebec2ea2fe3774cf4f512812870a988ca8ba14
tjgillweb <tjgillweb@gmail.com>
2020-12-24 18:08:46 +0530

    fetch jobs

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -3,8 +3,32 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async() => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if(loading){
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  return <h2>Jobs</h2>
 }
 
 export default App
